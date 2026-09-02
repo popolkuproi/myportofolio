1de0e95e5f3d61d06a914acaafb1524a3b833ca5
popolkuproi <naufalhabibullah94@gmail.com>
2026-09-02 11:26:19 +0700

    Initial Django project setup

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myportofolio project.
+URL configuration for myportfolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
